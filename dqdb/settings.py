9b8697af9c75952929e32f80a31c589a4a86fdb9
kqr <k@rdw.se>
2015-06-24 14:23:25 +0000

    made settings actually work with allowed hosts

--- dqdb/settings.py
+++ dqdb/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DQDB_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DQDB_DEBUG', False)
 
-ALLOWED_HOSTS = os.environ.get('DQDB_ALLOWED_HOSTS', ['localhost', '127.0.0.1'])
+ALLOWED_HOSTS = os.environ.get('DQDB_ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 
 # Application definition
